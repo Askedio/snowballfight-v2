6edf2c1c8659c45d9f799cdb73548c2204dbbee2
Will Bowman <gcphost@gmail.com>
2025-01-28 13:12:30 -0800

    Add back os

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -1,10 +1,12 @@
+const os = require('node:os');
+
 module.exports = {
     apps: [{
         name: "colyseus-app",
         script: 'lib/index.js',
         time: true,
         watch: false,
-        instances: 1,
+        instances: os.cpus().length,
         exec_mode: 'fork',
         wait_ready: true,
         env_production: {
