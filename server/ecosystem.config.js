944a51c78ce27b2c103420d367429e9337b5d26d
Will Bowman <gcphost@gmail.com>
2025-01-28 11:45:34 -0800

    Set things up for hosting?

---
+++ server/ecosystem.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    apps: [{
+        name: "colyseus-app",
+        script: 'build/index.js',
+        time: true,
+        watch: false,
+        instances: os.cpus().length,
+        exec_mode: 'fork',
+        wait_ready: true,
+        env_production: {
+            NODE_ENV: 'production'
+        }
+    }],
+};
