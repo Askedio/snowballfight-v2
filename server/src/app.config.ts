5f7124112e8cc6a961d712fba47fc69abdf1d530
Will Bowman <gcphost@gmail.com>
2025-02-02 12:38:52 -0800

    Proxy?

--- server/src/app.config.ts
+++ server/src/app.config.ts
@@ -99,7 +99,7 @@ export default config({
     app.use(express.static(path.join(__dirname, "../../client/dist")));
 
    
-    app.get("/.proxy/", (req, res) => {
+    app.get("/.proxy/api", (req, res) => {
       res.sendFile(path.join(__dirname, "../../client/dist", "index.html"));
     });
 
